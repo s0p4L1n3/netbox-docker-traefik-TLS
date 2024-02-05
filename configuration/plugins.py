ce4d12a975eaa2485ab3c03e2c6efbfb6817e2f3
s0p4L1n3 <126569468+s0p4L1n3@users.noreply.github.com>
2024-02-05 08:56:56 +0100

    Add files via upload

---
+++ configuration/plugins.py
@@ -0,0 +1,26 @@
+# Add your plugins and plugin settings here.
+# Of course uncomment this file out.
+
+# To learn how to build images with your required plugins
+# See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
+
+# PLUGINS = ["netbox_bgp"]
+
+# PLUGINS_CONFIG = {
+#   "netbox_bgp": {
+#     ADD YOUR SETTINGS HERE
+#   }
+# }
+
+
+#PLUGINS = [
+#    'netbox_napalm_plugin'
+#]
+
+
+#PLUGINS_CONFIG = {
+#    'netbox_napalm_plugin': {
+#        'NAPALM_USERNAME': 'napalm',
+#        'NAPALM_PASSWORD': '',
+#    },
+#}
